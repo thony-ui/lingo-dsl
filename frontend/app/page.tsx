fad2acbad54be284bde13b120f7587fda4216367
thony-ui <anthonyhermanto1@gmail.com>
2026-02-28 14:21:17 +0800

    Add github link

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           </div>
           <div className="flex items-center gap-4">
             <Button variant="ghost" size="sm" asChild>
-              <Link href="https://github.com" target="_blank">
+              <Link href="https://github.com/thony-ui/lingo-dsl" target="_blank">
                 <Github className="w-4 h-4 mr-2" />
                 GitHub
               </Link>
@@ -59,7 +59,7 @@ export default function Home() {
               </Link>
             </Button>
             <Button size="lg" variant="outline" className="text-lg px-8 py-6" asChild>
-              <Link href="https://github.com" target="_blank">
+              <Link href="https://github.com/thony-ui/lingo-dsl" target="_blank">
                 <Github className="w-5 h-5 mr-2" />
                 View on GitHub
               </Link>
